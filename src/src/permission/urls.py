0d61869f1f4d38fda3ae20994a845be41c46f47e
ja-deleon <95125440+ja-deleon@users.noreply.github.com>
2021-11-29 12:44:00 +0800

    fixed directory structure

--- src/src/permission/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import get_permissions
-
-urlpatterns = [
-    path('permissions/', get_permissions),
-]
--- src/src/role/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import get_roles
-
-urlpatterns = [
-    path('roles/', get_roles),
-]
